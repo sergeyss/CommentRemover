de1e1d199a96fdb8774cfa561115699b0d987d16
Sergey Valiev <123@456.email>
2022-03-09 15:58:00 +0300

    external path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,48 @@
 import com.commentremover.app.CommentProcessor;
 import com.commentremover.app.CommentRemover;
-import com.commentremover.exception.CommentRemoverException;
+
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args) throws CommentRemoverException {
 
-        // example for externalPath
+    private static String DEFAULT_PATH = "./";
+
+    public static void main(String[] args) throws Exception {
+
+        String externalPath = DEFAULT_PATH;
+        if (args.length > 0) {
+            File path = new File(args[0]);
+            if (path.exists() && path.isDirectory())
+                externalPath = args[0];
+        }
+
+
 
         CommentRemover commentRemover = new CommentRemover.CommentRemoverBuilder()
-                .removeJava(true) // Remove Java file Comments....
-                .removeJavaScript(true) // Remove JavaScript file Comments....
+                .removeJava(true) 
+                .removeJavaScript(true) 
                 .removeKotlin(true)
                 .removeProperties(true)
                 .removeCSS(true)
                 .removeXML(true)
                 .removeHTML(true)
-                .removeJSP(true) // etc..
-                .removeTodos(true) // Remove todos
-                .removeSingleLines(true) // Do not remove single line type comments
-                .removeMultiLines(true) // Remove multiple type comments
-                .preserveJavaClassHeaders(false) // Preserves class header comment
-                .preserveCopyRightHeaders(false) // Preserves copyright comment
-                .startExternalPath("/Users/vlad/IdeaProjects/crs-system")// Give it full path for external directories
-                //.setExcludePackages(new String[]{"src.main.java.model"}) // Refers to /Users/user/Projects/MyOtherProject/src/main/java/model and skips this directory.
+                .removeJSP(true) 
+                .removeTodos(true) 
+                .removeSingleLines(true) 
+                .removeMultiLines(true) 
+                .preserveJavaClassHeaders(false) 
+                .preserveCopyRightHeaders(false) 
+                .startExternalPath(externalPath)
+
                 .build();
 
         CommentProcessor commentProcessor = new CommentProcessor(commentRemover);
         commentProcessor.start();
+
+        removeTestFolders();
+    }
+
+    private static void removeTestFolders() {
+
     }
 }
