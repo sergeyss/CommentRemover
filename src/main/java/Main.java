fa04991f743a9d0c5967622529258eb5bfb3a758
Sergey Valiev <123@456.email>
2022-03-09 17:10:23 +0300

    remove tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import com.commentremover.app.CommentProcessor;
 import com.commentremover.app.CommentRemover;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Main {
 
@@ -16,33 +18,49 @@ public class Main {
                 externalPath = args[0];
         }
 
-
-
         CommentRemover commentRemover = new CommentRemover.CommentRemoverBuilder()
-                .removeJava(true) 
-                .removeJavaScript(true) 
+                .removeJava(true)
+                .removeJavaScript(true)
                 .removeKotlin(true)
                 .removeProperties(true)
                 .removeCSS(true)
                 .removeXML(true)
                 .removeHTML(true)
-                .removeJSP(true) 
-                .removeTodos(true) 
-                .removeSingleLines(true) 
-                .removeMultiLines(true) 
-                .preserveJavaClassHeaders(false) 
-                .preserveCopyRightHeaders(false) 
+                .removeJSP(true)
+                .removeTodos(true)
+                .removeSingleLines(true)
+                .removeMultiLines(true)
+                .preserveJavaClassHeaders(false)
+                .preserveCopyRightHeaders(false)
                 .startExternalPath(externalPath)
-
+                //.setExcludePackages(new String[]{"src.main.java.model"}) // Refers to /Users/user/Projects/MyOtherProject/src/main/java/model and skips this directory.
                 .build();
 
         CommentProcessor commentProcessor = new CommentProcessor(commentRemover);
-        commentProcessor.start();
+        //commentProcessor.start();
 
-        removeTestFolders();
+        removeTestFolders(externalPath);
     }
 
-    private static void removeTestFolders() {
+    private static void removeTestFolders(String path) throws IOException {
+        Files.walk(new File(path).toPath())
+                .filter(file -> file.toFile().isDirectory() && file.getFileName().toString().equals("test"))
+                .forEach(file -> {
+                    try {
+                        deleteRecursively(file.toFile());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
 
+    private static void deleteRecursively(File file) throws IOException {
+        File[] files = file.listFiles();
+        if (file.isDirectory() && files != null && files.length > 0) {
+            for (File fileInDirectory : files)
+                deleteRecursively(fileInDirectory);
+            file.delete();
+        } else
+            file.delete();
     }
 }
